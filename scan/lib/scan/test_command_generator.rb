6976eb9afe1553bf0b29e7a91c206c715b02aedc
Kajornsak Peerapathananont <kajornsak_psm@hotmail.com>
2023-11-20 00:42:32 +0700

    test without substitution

--- scan/lib/scan/test_command_generator.rb
+++ scan/lib/scan/test_command_generator.rb
@@ -119,7 +119,7 @@ module Scan
     end
 
     def pipe
-      pipe = [" 2> >(grep -v 'createItemModels' >&2) | tee '#{xcodebuild_log_path}'"]
+      pipe = ["2> temp_err.log; cat temp_err.log | grep -v 'createItemModels' 1>&2; rm temp_err.log | tee '#{xcodebuild_log_path}'"]
 
       filter_patterns = [
         "createItemModels",
